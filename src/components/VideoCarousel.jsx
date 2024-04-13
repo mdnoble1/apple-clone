efec35d4fb20be422f8919f7f6abec0649ed8938
Md Noble <mdnoble.wd@gmail.com>
2024-04-14 00:30:05 +0600

    maped all slider data on video carousel component

--- src/components/VideoCarousel.jsx
+++ src/components/VideoCarousel.jsx
@@ -1,8 +1,18 @@
-
+import { hightlightsSlides } from "../constants/Index"
 
 const VideoCarousel = () => {
   return (
-    <div>VideoCarousel</div>
+    <>
+        <div className="flex items-center">
+            {hightlightsSlides.map(( list, i ) => (
+                <div key={list.id} id="slider" className="sm:pr-20 pr-10">
+                    <div className="video-carousel_container">
+                        <div className="w-full h-full flex-center rounded-3xl overflow-hidden bg-black"></div>
+                    </div>
+                </div>
+            ))}
+        </div>
+    </>
   )
 }
 
