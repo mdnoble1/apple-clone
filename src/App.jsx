bd9ce7ab35c997626d3fba08abfb9f9c3c1e16a5
Md Noble <mdnoble.wd@gmail.com>
2024-04-13 10:01:00 +0600

    styled the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Hero from "./components/Hero";
 import Highlights from "./components/Highlights";
 import Navbar from "./components/Navbar";
 
+
 const App = () => {
   return (
     <main className="bg-black">
--- src/constants/index.js
+++ src/constants/index.js
@@ -7,7 +7,7 @@ import {
     highlightThirdVideo,
     whiteImg,
     yellowImg,
-  } from "../utils";
+  } from "../utils/Index.js";
   
   export const navLists = ["Store", "Mac", "iPhone", "Support"];
   
