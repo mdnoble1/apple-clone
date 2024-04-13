efd025a19e5bd22e4af63dafb81797ef5f45719f
Md Noble <mdnoble.wd@gmail.com>
2024-04-13 10:44:30 +0600

    added a video on banner

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,57 @@
-
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { heroVideo, smallHeroVideo } from "../utils/Index";
+import { useState } from "react";
+import { useEffect } from "react";
 
 const Hero = () => {
+  const [videoSrc, setVideoSrc] = useState(
+    window.innerWidth < 760 ? smallHeroVideo : heroVideo
+  );
+
+  const handleVideoSrcSet = () => {
+    if(window.innerWidth < 760 ){
+      setVideoSrc(smallHeroVideo);
+    }else{
+      setVideoSrc(heroVideo);
+    }
+  }
+
+
+  useEffect( () => {
+    window.addEventListener("resize", handleVideoSrcSet);
+
+    return () => {
+      window.removeEventListener("resize", handleVideoSrcSet);
+    }
+  }, [])
+
+  useGSAP(() => {
+    gsap.to("#hero", {
+      opacity: 1,
+      delay: 1.5,
+    });
+  }, []);
+
   return (
     <section className="w-full nav-height bg-black relative">
+      {/* video part  */}
       <div className="h-5/6 w-full flex-col flex-center">
-        <p className="hero-title">Iphone 15 Pro</p>
+        <p id="hero" className="hero-title">
+          iphone 15 Pro
+        </p>
+        <div className="w-9/12 md:w-10/12">
+          <video className="pointer-events-none" autoPlay muted playsInline={true} key={videoSrc}>
+            <source src={videoSrc} type="video/mp4"/>
+          </video>
+        </div>
+      </div>
+      {/* call to action part  */}
+      <div>
+        
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
