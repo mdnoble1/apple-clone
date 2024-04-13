2d5953c6c15d931d063be251b45fe0c29f6c63be
Md Noble <mdnoble.wd@gmail.com>
2024-04-13 19:07:18 +0600

    created highlights section and added heading

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,9 +1,46 @@
-
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { rightImg, watchImg } from "../utils/Index";
 
 const Highlights = () => {
-  return (
-    <div>Highlights</div>
-  )
-}
+  useGSAP(() => {
+    gsap.to("#title", {
+      opacity: 1,
+      y: 0,
+    });
 
-export default Highlights
+    gsap.to(".link", {
+      opacity: 1,
+      y: 0,
+      duration: 1,
+      stagger: 0.25
+    });
+  }, []);
+
+  return (
+    <section
+      id="highlights"
+      className="w-screen overflow-hidden h-full common-padding bg-zinc"
+    >
+      <dir className="screen-max-width">
+        <div className="mb-12 w-full md:flex items-end justify-between">
+          <h1 id="title" className="section-heading">
+            Get the highlights.
+          </h1>
+          <div className="flex flex-wrap items-end gap-5">
+            <p className="link">
+              Watch the film
+              <img className="ml-2" src={watchImg} alt="watch" />
+            </p>
+            <p className="link">
+              Watch the event
+              <img className="ml-2 mt-1" src={rightImg} alt="right" />
+            </p>
+          </div>
+        </div>
+      </dir>
+    </section>
+  );
+};
+
+export default Highlights;
