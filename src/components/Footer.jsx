8bbd0b322e298cce429efe50d5a979d0f83cf484
Md Noble <mdnoble.wd@gmail.com>
2024-04-18 10:27:56 +0600

    added style on footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,5 @@
+import { footerLinks } from "../constants/Index";
+
 const Footer = () => {
   return (
     <footer className="py-5 sm:px-10 px-5">
@@ -5,10 +7,29 @@ const Footer = () => {
         <div>
           <p className="font-semibold text-gray text-xs">
             More ways to shop:{" "}
-            <span className="underline text-blue">Find an Apple Store.</span>
-            {" "} or <span className="underline text-blue">other retailer</span> near
+            <span className="underline text-blue">Find an Apple Store.</span> or{" "}
+            <span className="underline text-blue">other retailer</span> near
             you.
           </p>
+          <p className="font-semibold text-gray text-xs">
+            or call 0123-456-789
+          </p>
+        </div>
+        <div className="bg-neutral-700 my-5 h-[1px] w-full" />
+        <div className="flex flex-row md:flex-col justify-between md:items-center">
+          <p className="font-semibold text-gray text-xs">
+            Copyright @ 2024 Apple Inc. All rights reserved.
+          </p>
+          <div className="flex">
+            {footerLinks.map((link, i) => (
+              <p key={link} className="font-semibold text-gray text-xs">
+                {link}{" "}
+                {i !== footerLinks.length - 1 && (
+                  <span className="mx-2"> | </span>
+                )}
+              </p>
+            ))}
+          </div>
         </div>
       </div>
     </footer>
